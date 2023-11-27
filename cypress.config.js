dbf27159f15020439b21e926dd104c0504d17948
Catarina Albardeiro Bahia <catarina.bahia@mococa.com>
2023-11-27 08:52:13 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -7,9 +7,6 @@ module.exports = defineConfig({
     env: {
       viewportWidthBreakpoint: 768,
     },
-    defaultCommandTimeout: 10000,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
-});
+  projectId: 'vkbzg3'
+})
